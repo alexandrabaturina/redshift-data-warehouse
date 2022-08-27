853ad6edd001cfea486571fa71b14c4f1fc148af
alexandrabaturina <alexandra.v.baturina@gmail.com>
2022-08-26 17:09:59 -0700

    refactor: add docstrings

--- create_tables.py
+++ create_tables.py
@@ -4,22 +4,50 @@ from sql_queries import create_table_queries, drop_table_queries
 
 
 def drop_tables(cur, conn):
+    """
+    Function to drop each table using queries in drop_table_queries list.
+
+    Args:
+        cur (refcursor): Cursor to execute database queries
+        conn (object): Database connection object
+    Returns:
+        no value
+    """
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def create_tables(cur, conn):
+    """
+    Function to create each table using queries in create_table_queries list.
+
+    Args:
+        cur (refcursor): Cursor to execute database queries
+        conn (object): Database connection object
+    Returns:
+        no value
+    """
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    """
+    Function to parse dwh.cfg file, establish connection to the database,
+    drop existing tables, and create new ones.
+
+    Args:
+        no value
+    Returns:
+        no value
+    """
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
-    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
+    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}"
+                            .format(*config['CLUSTER'].values()))
     cur = conn.cursor()
 
     drop_tables(cur, conn)
